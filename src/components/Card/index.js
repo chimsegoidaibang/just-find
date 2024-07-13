89c6bc1008929f6cfec25ff42f0e0b87c22e2b82
hocondangyeu <hocondangyeu@hocondangyeu.local>
2024-07-14 04:00:57 +0700

    first commit on day 01

---
+++ src/components/Card/index.js
@@ -0,0 +1,16 @@
+const Card = () => {
+    return (
+        <div className='card'>
+            <div className='card-inner'>
+                <div className='font'>
+                    <h2>Font</h2>
+                </div>
+                <div className='back'>
+                    <h2>Back</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
